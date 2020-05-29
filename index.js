0e35c070d7d9fc1dea1fb77d0c7a43073c6c8116
Trinity Polk <polktr@oregonstate.edu>
2020-05-29 12:11:41 -0700

    New organization idea for JS

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
 
 var express = require('express');
-var mysq= require('./dbconfig.js')
+var mysql = require('./dbconfig.js');
 
-var app = express()
-var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
 
-app.engine('handlebars', handlebars.engine);
+var app = express();
+var handlebars = require('express-handlebars');
+app.engine('handlebars', handlebars());
 app.set('view engine','handlebars');
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.json);
+
+app.use('static', express.static('public'));
+app.use('/', express.static('public'));
+
 app.set('port', process.argv[2]);
+
+app.set('mysql', mysql);
+
+app.get('/', function(req, res){
+	res.render('index');
+});
+
+//Add specific ones here
+// app.use('/classes', require('./classes.js'));
+//
+// app.use('/search', require('./search.js'));
+//
+// app.use('/admin', require('./admin.js'));
+
+app.use(function(req, res){
+	res.status(404);
+	res.render('404');
+});
+
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.type('plain/text');
+	res.status(500);
+	res.render('500');
+});
+
 
 // app.get('/',function(req,res,next){
 //
@@ -22,12 +56,11 @@ app.set('port', process.argv[2]);
 //   //});
 // });
 
-app.use(express.static('public'));
 
-app.get('*', function (req, res, next)
-{
-	res.status(404).render('404');
-});
+// app.get('*', function (req, res, next)
+// {
+// 	res.status(404).render('404');
+// });
 
 app.listen(app.get('port'), function(){
   console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
