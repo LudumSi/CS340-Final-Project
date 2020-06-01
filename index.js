cf39854a6fa1cea846ac7592bb5ca4380e70ec74
Shadowizard <Neomaster2300@gmail.com>
2020-06-01 14:29:16 -0700

    Multiple materials and enchantments supported

--- index.js
+++ index.js
@@ -60,9 +60,6 @@ app.get('/editprofile',function(req,res,next){
 	res.render('editprofile');
 });
 
-
-
-
 //Database Time!!:
 //Ex to test Database --> Puts all blacksmith's names into the console = WORKS
 // mysql.pool.query("SELECT fname FROM blacksmith", function(err, result, fields){
@@ -70,10 +67,75 @@ app.get('/editprofile',function(req,res,next){
 // 	console.log(result);
 // });
 
+//Hold onto your hats boyos, this is going to get messy -P
 app.get('/weapon',function(req,res,next){
-  mysql.pool.query('SELECT * from weapon JOIN wpn_mat ON (weapon.name =  wpn_mat.wpn_name) JOIN wpn_magic ON (weapon.name=wpn_magic.wpn_name) JOIN blacksmith ON (weapon.smth_id=blacksmith.id) GROUP BY name', function(err, weapon, fields){
-		res.render('weapon',{data: weapon});
+
+	//mysql.pool.query('SELECT * from weapon JOIN wpn_mat ON (weapon.name =  wpn_mat.wpn_name) JOIN wpn_magic ON (weapon.name=wpn_magic.wpn_name) JOIN blacksmith ON (weapon.smth_id=blacksmith.id) GROUP BY name', function(err, weapon, fields){
+		mysql.pool.query('SELECT name, type, cost FROM weapon', function(err, weapon_names, fields){
+
+			mysql.pool.query('SELECT wpn_name, mat_name FROM wpn_mat', function (err, wpn_mats, fields){
+
+				mysql.pool.query('SELECT * FROM wpn_magic', function (err, wpn_magics, fields){
+
+					mysql.pool.query('SELECT name,fname,lname FROM weapon JOIN blacksmith ON (weapon.smth_id=blacksmith.id) GROUP BY name', function (err, smith_names, fields){
+
+						//console.log(wpn_mats);
+						weapon_data = [];
+
+						for(var i = 0;i < weapon_names.length;i++){
+							var	weapon_packet = {};
+							weapon_packet.name = weapon_names[i].name;
+							weapon_packet.materials = [];
+							weapon_packet.enchants = [];
+							weapon_packet.type = weapon_names[i].type;
+							weapon_packet.cost = weapon_names[i].cost;
+							weapon_data.push(weapon_packet);
+						}
+
+						for(var i = 0;i < wpn_mats.length;i++){
+
+							for(var j = 0;j < weapon_data.length;j++){
+
+								if(wpn_mats[i].wpn_name == weapon_data[j].name){
+
+									weapon_data[j].materials.push(wpn_mats[i].mat_name);
+								}
+							}
+						}
+
+						for(var i = 0;i < wpn_magics.length;i++){
+
+							for(var j = 0;j < weapon_data.length;j++){
+
+								if(wpn_magics[i].wpn_name == weapon_data[j].name){
+
+									weapon_data[j].enchants.push(wpn_magics[i].magic_name);
+								}
+							}
+						}
+
+						for(var i = 0;i < smith_names.length;i++){
+
+							for(var j = 0;j < weapon_data.length;j++){
+
+								if(smith_names[i].name == weapon_data[j].name){
+
+									weapon_data[j].fname = smith_names[i].fname;
+									weapon_data[j].lname = smith_names[i].lname;
+								}
+							}
+						}
+
+						//console.log(weapon_data);
+						res.render('weapon',{data: weapon_data})
+
+					});
+
+				});
+
+			});
 	});
+
 });
 
 app.get('/enchant',function(req,res,next){
@@ -93,9 +155,6 @@ app.get('/profile',function(req,res,next){
 		res.render('profile',{data: blacksmiths});
 	});
 });
-
-
-
 
 app.use(function(req,res){
   res.status(404);
